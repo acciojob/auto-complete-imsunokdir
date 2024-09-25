89de8072c60e5ea7b88a9153ffea0e2181b5d334
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-09-25 11:32:54 +0530

    add all features

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const fruits = [
+  "apple",
+  "banana",
+  "cherry",
+  "date",
+  "elderberry",
+  "fig",
+  "bandf",
+];
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [inputValue, setInputValue] = useState("");
+  const [suggestion, setSuggestion] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default App
+  useEffect(() => {
+    if (inputValue.trim() === "") {
+      setSuggestion([]);
+      return;
+    }
+    const fetchSuggestion = setTimeout(() => {
+      const filtered = fruits.filter((fruit) =>
+        fruit.toLowerCase().startsWith(inputValue.toLowerCase())
+      );
+      setSuggestion(filtered);
+    }, 150);
+    return () => clearTimeout(fetchSuggestion);
+  }, [inputValue]);
+
+  return (
+    <div id="main">
+      {/* Do not remove the main div */}
+      <input
+        type="text"
+        placeholder="search item's"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <ul>
+        {suggestion.length > 0 &&
+          suggestion.map((fruit, i) => {
+            return <li key={i}>{fruit}</li>;
+          })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
